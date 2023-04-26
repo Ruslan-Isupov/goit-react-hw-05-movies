dfe73ea31fb6b116fd9a116949e3e9975c9a2085
Ruslan-Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2023-04-26 16:05:35 +0000

    Deploying to gh-pages from @ Ruslan-Isupov/goit-react-hw-05-movies@7431f5a8c2c04f2bd9af292bb3c1cd3518d4de94 🚀

---
+++ static/js/520.a39776a8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,n){n.r(t);var o=n(184);t.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("p",{children:"Not Found"}),(0,o.jsx)("img",{src:"src\\components\\pageNotFou.nd.jpg",alt:"not found page"})]})}}}]);
+//# sourceMappingURL=520.a39776a8.chunk.js.map
